68e3b5a8224a8c83e4822d1a4a2844c13c022c22
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2023-03-24 13:26:35 -0500

    Updated Homework7

--- Image Processing/Homework7.py
+++ Image Processing/Homework7.py
@@ -3,7 +3,8 @@ import cv2
 
 coin = cv2.imread("Coins/coin1.png")
 cv2.cvtColor(coin, cv2.COLOR_BGR2HSV)
-mask = np.logical_and((coin[:,:,0] > 5), coin[:,:,0] < 40)
+#mask = np.logical_and((coin[:,:,0] > 5), coin[:,:,0] < 40)
+mask = coin [:,:,1] < 180
 mask = np.uint8(mask * 255)
 mask[mask < 128] = 0
 mask[mask > 127] = 255
