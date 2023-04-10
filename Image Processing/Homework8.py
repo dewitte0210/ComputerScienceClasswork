8a179576fe9100cd82c327cf426c772cdb1c6615
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2023-04-09 20:15:11 -0500

    Worked on Homework 8

--- Image Processing/Homework8.py
+++ Image Processing/Homework8.py
@@ -2,17 +2,49 @@ import numpy as np
 import cv2
 from skimage.transform import radon
 
+
+
 #img = cv2.imread("standard_test_images/quadrilateral.png", cv2.IMREAD_GRAYSCALE)
 img = cv2.imread("standard_test_images/quadrilateral.png", cv2.IMREAD_GRAYSCALE)
+print(img.shape)
 rImg = radon(img, np.linspace(0.0, 180.0, num=540, endpoint=False))
 rImg = rImg / np.max(rImg)
 rImg[rImg < 0.3] = 0
-rImg
-print(img.shape)
-print(rImg.shape)
+rImg[rImg != 0 ] = 255
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+rImg = cv2.erode(rImg,kernel,iterations = 2)
+lines = []
+for i in range(len(rImg)):
+    for j in range(len(rImg[1])):
+        if rImg[i,j] == 255:
+            p, theta = i, j/3
+            duplicate = False;
+            for n in range(len(lines)):
+                if (p > lines[n][0] - 5 and p < lines[n][0] + 5) or (theta > lines[n][1] - 5 and theta < lines[n][1] + 5):
+                    duplicate = True
+            if not duplicate:
+                lines.append((p, theta))
 
 #Detecting the lines
+for i in range(len(lines)):
+    coords = (lines[i][0] * np.cos(lines[i][1]), lines[i][0] * np.sin(lines[i][1]))
+    print(coords)
+    slope = np.float32(coords[0] / coords[1]) * -1 # this is the slope of our perpendicular line
+    b = (slope * coords[0]  *-1) + coords[1]
+    lines[i] = (slope, b)
 
+print(lines)    
+intersects = []
+for i in range(len(lines)):
+    for k in range(len(lines)):
+        x = (lines[i][1] - lines[k][1]) / (lines[i][0] - lines[k][0])
+        y = lines[i][0] * x + lines[i][1]
+        if x > 0 and x < img.shape[0] and y > 0 and y < img.shape[1]:
+            intersects.append((x,y))
+
+print(intersects)
+
+    
 cv2.imshow("Quadrilateral", img)
 cv2.imshow("Radon Transform", rImg)
 cv2.waitKey(0)
--- Image Processing/LegoMosaic.py
+++ Image Processing/LegoMosaic.py
@@ -6,41 +6,27 @@ from PIL import Image
 legoCSV = pd.read_csv(r"LEGOColors.csv")
 legoRGB = np.float32(pd.DataFrame(legoCSV, columns=['Red','Green','Blue']).to_numpy() / 255.0)
 num_colors = legoRGB.shape[0]
+HEIGHT = 128.0
 
-HEIGHT = 112.0
-
-img = cv2.imread("standard_test_images/rose.png")
+img = cv2.imread("standard_test_images/clocktower.jpg")
 H, W = img.shape[:2]
 img = Image.fromarray(img)
 img = img.resize((round(HEIGHT * W / H), round(HEIGHT)), Image.LANCZOS)
 img = (np.float32(img) / 255.0)
 height, width = img.shape[:2]
 
-kernel = np.array([[0,0,7], [3,5,1.0]]) / 16
-for i in range(height):
-    for j in range(width):
-        b, g, r = img[i,j]
-        for x in range(num_colors):
-            if(legoRGB[0, x] < r ):
-                bot = r - legoRGB[0,x]
-                top = legoRGB[0,x+1] - r
-                r = bot if bot<top else top
-            if(legoRGB[1,x] < g):
-                bot = g - legoRGB[1,x]
-                top = legoRGB[1,x+1] - g
-                g = bot if bot<top else top
-            if(legoRGB[2,x] < b):
-                bot = b - legoRGB[2,x+1]
-                top = legoRGB[2,x+1] - b
-                b = bot if bot<top else top
-        bErr = img[i,j,0] - b
-        gErr = img[i,j,1] - g
-        rErr = img[i,j,2] - r  
-        img[i:i+2, j-1:j+2, 0] += (bErr * kernel)
-        img[i:i+2, j-1:j+2,1] += (gErr * kernel)
-        img[i:i+2, j-1:j+2,2] += (rErr * kernel)
-        img[i,j] = b, g, r
-
+for i in range(1,height - 1):
+    for j in range(1,width - 1):
+        min_dist = np.argmin(np.sum((img[i,j] - legoRGB[:,::-1]) ** 2,axis=1))
+        value = legoRGB[min_dist, ::-1]
+        error = img[i,j] - value
+        img[i, j + 1] += error * 7/16
+        img[i + 1, j-1] += error * 3/16
+        img[i + 1, j] += error * 5/16
+        img[i + 1, j+1] += error / 16
+        img[i,j] = value
+img = np.uint8(img * 255)
+img = img.repeat(4, 0).repeat(4,1)
 cv2.imshow("Lets Hope", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
