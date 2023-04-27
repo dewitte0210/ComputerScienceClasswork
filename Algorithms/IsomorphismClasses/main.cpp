54255a17c24f5f7811a8a4a8114f06aac41566de
dewitte0210 <dewitte0210@my.uwstout.edu>
2023-04-26 20:56:19 -0500

    worked on Cha:Isomorphism classes

---
+++ Algorithms/IsomorphismClasses/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+std::vector<boolean> addEdge(){
+  
+
+}
+
+int main(){
+  int count;
+  cout << "Enter the count you wish to calculate" << endl;
+  cin >> count;
+  int total = 0;
+  for(int i = 0; i < count; i++)
+  {
+    
+  }
+  return 0;
+}
